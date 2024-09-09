e9a3c824ac9066bcb0e8087266c998d210782803
ogbesgt <Godstime.osemenkhian@mtn.com>
2024-09-09 15:43:38 +0100

    added execution role

--- ecs-deploy/main.tf
+++ ecs-deploy/main.tf
@@ -1,6 +1,6 @@
 
 provider "aws" {
-  region = "eu-west-2" 
+  region = "eu-west-2"
 }
 
 
@@ -8,14 +8,36 @@ resource "aws_ecs_cluster" "nextpoc" {
   name = "nextpoc"
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role"
 
-resource "aws_security_group" "ecs_sg" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
+resource "aws_security_group" "nextpoc" {
   name   = "nextpoc"
-  vpc_id = "vpc-01a0cb45befe50f0b"  
+  vpc_id = "vpc-01a0cb45befe50f0b"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -33,12 +55,13 @@ resource "aws_ecs_task_definition" "my_task" {
   family                   = "my-ecs-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"  
-  memory                   = "512"  
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([{
     name      = "nextjspoc"
-    image     = ""  
+    image     = ""
     essential = true
     portMappings = [{
       containerPort = 3000
@@ -55,10 +78,11 @@ resource "aws_ecs_service" "nextpoc_svc" {
   task_definition = aws_ecs_task_definition.my_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
-  
+
   network_configuration {
-    subnets         = ["subnet-0980118215d96e765", "subnet-0b64a78400aee6ca5"]  
-    security_groups = [aws_security_group.nextpoc.id]
+    subnets          = ["subnet-0980118215d96e765", "subnet-0b64a78400aee6ca5"]
+    security_groups  = [aws_security_group.nextpoc.id]
     assign_public_ip = true
   }
 }
+
