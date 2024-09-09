14c5ee312bb86803849a19c4f83cb61274cfa07b
ogbesgt <Godstime.osemenkhian@mtn.com>
2024-09-09 15:30:36 +0100

    created .tf for ecs

--- ecs-deploy/main.tf
+++ ecs-deploy/main.tf
@@ -1,17 +1,17 @@
-# 1. Providers Configuration (Make sure to have your AWS credentials configured)
+
 provider "aws" {
-  region = "eu-west-2"  # Change to your preferred region
+  region = "eu-west-2" 
 }
 
-# 2. Define ECS Cluster
-resource "aws_ecs_cluster" "my_cluster" {
-  name = "my-ecs-cluster"
+
+resource "aws_ecs_cluster" "nextpoc" {
+  name = "nextpoc"
 }
 
-# 3. Define Security Group to allow inbound traffic (for example, HTTP)
+
 resource "aws_security_group" "ecs_sg" {
-  name   = "ecs_security_group"
-  vpc_id = "vpc-01a0cb45befe50f0b"  # Replace with your VPC ID
+  name   = "nextpoc"
+  vpc_id = "vpc-01a0cb45befe50f0b"  
 
   ingress {
     from_port   = 80
@@ -28,17 +28,17 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
-# 4. Define ECS Task Definition
+
 resource "aws_ecs_task_definition" "my_task" {
   family                   = "my-ecs-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"  # 256 CPU units (~0.25 vCPU)
-  memory                   = "512"  # 512 MiB of memory
+  cpu                      = "256"  
+  memory                   = "512"  
 
   container_definitions = jsonencode([{
     name      = "nextjspoc"
-    image     = "983883494204.dkr.ecr.eu-west-2.amazonaws.com/nextjspoc1d09dd0a82/nextjsfunction032c9ef3repo:nextjsfunction-e331b061856f-v1"  # Example: NGINX image
+    image     = ""  
     essential = true
     portMappings = [{
       containerPort = 3000
@@ -48,17 +48,17 @@ resource "aws_ecs_task_definition" "my_task" {
   }])
 }
 
-# 5. ECS Service
+
 resource "aws_ecs_service" "nextpoc_svc" {
   name            = "nextpoc_svc"
-  cluster         = aws_ecs_cluster.my_cluster.id
+  cluster         = aws_ecs_cluster.nextpoc.id
   task_definition = aws_ecs_task_definition.my_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
   
   network_configuration {
-    subnets         = ["subnet-xxxxxx", "subnet-yyyyyy"]  # Replace with your subnet IDs
-    security_groups = [aws_security_group.ecs_sg.id]
+    subnets         = ["subnet-0980118215d96e765", "subnet-0b64a78400aee6ca5"]  
+    security_groups = [aws_security_group.nextpoc.id]
     assign_public_ip = true
   }
 }
